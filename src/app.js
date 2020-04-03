95f3d7fbf50a06e275dac06a9fcb96b79fbbc427
hujinbin <437044048@qq.com>
2020-04-03 18:04:41 +0800

    edit

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
 import Vue from 'vue'
 
+import index from './index'
+
+Vue.component('infiniteScroll',index)
+
+new Vue({
+    el:'#app',
+    data(){
+        return{ 
+            num:20
+        }
+    },
+    methods:{
+        loadMore(){
+            this.num = this.num + 20
+            console.log('加载次数='+(this.num/20))
+        },
+    }
+})
--- src/index.vue
+++ src/index.vue
@@ -1,10 +1,36 @@
 <template>
-  
+    <div class="infinite-scroll">
+        <slot/>
+    </div>
 </template>
 
 <script>
 export default {
- 
+    methods:{
+handleScroll () {
+       //变量scrollTop是滚动条滚动时，距离顶部的距离
+       const scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+       //变量windowHeight是可视区的高度
+       const windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
+       //变量scrollHeight是滚动条的总高度
+       const scrollHeight = document.documentElement.scrollHeight||document.body.scrollHeight;
+       //滚动条到底部的条件
+       if(scrollTop+windowHeight==scrollHeight){
+           this.loadMore() // 加载的列表数据
+       }
+   },
+//    触发加载
+   loadMore(){
+        this.$emit('infinite-scroll')
+    },
+    },
+ mounted () {
+     this.handleScroll();
+    window.addEventListener('scroll', this.handleScroll)
+},
+destroyed () {
+    window.removeEventListener('scroll', this.handleScroll)
+},
 };
 </script>
 
